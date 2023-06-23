56e3b88107db198d97caf392f323ed87482d6330
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2023-06-23 11:36:21 +0530

    Update reverse_of.py
    Changed to function

--- reverse_of.py
+++ reverse_of.py
@@ -1,10 +1,14 @@
+def reverse(name_rev):
+    temp=name_rev
+    temp2=""
+    length=len(name_rev)
+    for i in range(1,length+1):
+        temp2+=name_rev[length -i]
+    print("Reverse of word ::",temp2)
+    if temp == temp2:
+        print(temp,"is palindrome ")
+    else:
+        print (temp2 ,"is not palindrome ")
+
 name=input ("Enter the word ::")
-temp=name
-temp2=""
-length=len(name)
-for i in range(1,length+1):
-    sub=name[length-i]
-    temp2+=name[length -i]
-print("Reverse of word ::",temp2)
-if temp==temp2:
-   
+reverse(name)
