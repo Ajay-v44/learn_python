e26eefd6e54ea1bf5cdc09a2a7f886e579c301ca
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2023-06-26 15:00:39 +0530

    Create Square_rev.py

---
+++ Square_rev.py
@@ -0,0 +1,15 @@
+num_of_numbers = int(input("Enter the number of numbers: "))
+number_list = []
+squared_list = []
+
+i = 0
+while i < num_of_numbers:
+    num = int(input("Enter the number: "))
+    number_list.append(num)
+
+i=0
+while i < num_of_numbers:
+    num_squared = number_list[i] ** 2
+    squared_list.append(num_squared)
+    i += 1
+print(squared_list[::-1])
